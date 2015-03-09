059b8b3a55ba8e43c2826b095e201baca50dd218
Marcin0912 <computerapi@gmail.com>
2015-03-09 18:32:34 -0500

    Update init.php

--- core/init.php
+++ core/init.php
@@ -20,11 +20,11 @@ $GLOBALS['config'] = array (
     )
 );
 
-// This function standard php library autoload register
-// that takes anonymus function with a param $class,
-// $db = new DB(); will call this function with DB as the argument for $class
-// $user = DB::getInstance() same here DB gets passed to anonymus function as an argument
-// so the call looks like spl_autoload_register(require_once 'classes/DB.php');
+
+// Below function takes anonymus function with a param $class,
+// $hash = Cookie::get() - will call this function with DB as the argument for $class
+// $hashCheck = DB::getInstance() - same here DB gets passed to anonymus function as an argument.
+// The call looks like spl_autoload_register(require_once 'classes/DB.php');
 spl_autoload_register(function($class) {
     require_once 'classes/' . $class . '.php';
 });
